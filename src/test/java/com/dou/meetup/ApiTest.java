e676c5b7216d3616113b73107cf33241b5603f9d
Alberto <alberto.lopez@digitalonus.com>
2018-11-08 11:56:39 -0600

    Fixed parametrization

--- src/test/java/com/dou/meetup/ApiTest.java
+++ src/test/java/com/dou/meetup/ApiTest.java
@@ -5,6 +5,7 @@ import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -15,20 +16,20 @@ public class ApiTest {
 	
 	@Parameters({"url", "port"})
 	@BeforeTest(alwaysRun = true)
-	public void setUp(String url, String port) {
-		RestAssured.baseURI = "http://localhost";
-		RestAssured.port = 8090;
+	public void setUp(String url, @Optional("") String port) {
+		RestAssured.baseURI = url;
+		RestAssured.port = new Integer(port);
 	}
 	
-	
-	@Test(groups = "integration")
+	@Test(groups = "integration", priority = 1)
 	public void testInitialPage() {
+		
+		System.out.println("testApiInitialPage");
 		
 		when().
 			get("/").
 		then().
 			statusCode(200).
 			contentType(ContentType.TEXT);
-		
 	}
 }
--- src/test/java/com/dou/meetup/UiTest.java
+++ src/test/java/com/dou/meetup/UiTest.java
@@ -1,9 +1,12 @@
 package com.dou.meetup;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -11,25 +14,39 @@ public class UiTest {
 	
 	private WebDriver driver;
 
-	@Parameters("url")
-	@BeforeMethod
-	public void setup(String url) {
+	@Parameters({"url", "port"})
+	@BeforeMethod(alwaysRun = true)
+	public void setup(String url, @Optional("") String port) {
+		
+		String appUrl = url;
+		
+		if (port != null && !port.isEmpty()) {
+			appUrl += ":" + port;
+		}
+		
 		driver = new ChromeDriver();
-		driver.get(url);
+		driver.get(appUrl);
 	}
 	
-	@AfterMethod
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
 		driver.quit();
 	}
 	
-	@Test(groups = "integration")
+	@Test(groups = "integration", priority = 2)
 	public void testWebappTitle() {
 		
+		System.out.println("testUiWebappTitle");
+		
+		Assert.assertEquals(driver.getTitle(), "");
 	}
 	
-	@Test(groups = "integration")
+	@Test(groups = "integration", priority = 2)
 	public void testInitialMessage() {
 		
+		System.out.println("testUiInitialMessage");
+		
+		Assert.assertEquals(driver.findElement(By.tagName("body")).getText(), 
+				"Greetings from Spring Boot!");
 	}
 }
